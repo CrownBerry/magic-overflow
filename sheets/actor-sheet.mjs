5150278e7910920565c8df0b45d063ba02f3ee1a
Aleksandr Semenov <a.semenov.job@gmail.com>
2025-01-24 15:54:10 +0100

    Add more character fields to sheet

--- sheets/actor-sheet.mjs
+++ sheets/actor-sheet.mjs
@@ -1,38 +1,38 @@
 export class MagicOverflowActorSheet extends ActorSheet {
     static get defaultOptions() {
-        return foundry.utils.mergeObject(super.defaultOptions, {
-            template: "systems/magic-overflow/templates/actor-sheet.hbs",
-            classes: ["magic-overflow", "sheet", "actor"],
-            width: 600,
-            height: 700,
-            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
-        });
+      return foundry.utils.mergeObject(super.defaultOptions, {
+        template: "systems/magic-overflow/templates/actor-sheet.hbs",
+        classes: ["magic-overflow", "sheet", "actor"],
+        width: 600,
+        height: 700,
+        tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "skills" }]
+      });
     }
-
+  
     async getData() {
-        const data = await super.getData();
-        data.config = CONFIG.magicOverflow;
-        return data;
+      const context = await super.getData();
+      context.system = this.actor.system;
+      return context;
     }
-
+  
     activateListeners(html) {
-        super.activateListeners(html);
-
-        html.find(".skill-name").on("click", ev => {
-            const skill = ev.currentTarget.dataset.skill;
-            this.rollSkillCheck(skill);
-        });
+      super.activateListeners(html);
+      
+      html.find(".skill-name").on("click", ev => {
+        const skillKey = ev.currentTarget.dataset.skill;
+        this.rollSkillCheck(skillKey);
+      });
     }
-
-    rollSkillCheck(skill) {
-        const actor = this.actor;
-        const skillData = actor.system.skills[skill];
-        const dicePool = 1 + (skillData?.value || 0);
-
-        const roll = new Roll(`${dicePool}d8`).roll();
-        roll.toMessage({
-            speaker: ChatMessage.getSpeaker({ actor }),
-            flavor: `Rolling ${skill}`
-        });
+  
+    rollSkillCheck(skillKey) {
+      const skill = this.actor.system.skills[skillKey];
+      const dicePool = 1 + (skill?.value || 0);
+  
+      const roll = new Roll(`${dicePool}d8`).roll();
+      roll.toMessage({
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: `Rolling ${skill?.name || skillKey}`
+      });
     }
-}
+  }
+  
