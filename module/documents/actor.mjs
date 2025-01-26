8624fa7d407a148be3a3f9e9c7d9439a88b89628
Aleksandr Semenov <a.semenov.job@gmail.com>
2025-01-26 16:06:55 +0100

    Fixing actor

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -2,7 +2,7 @@ export class MagicOverflowActor extends Actor {
     prepareData() {
         super.prepareData();
         const actorData = this;
-        const systemData = this.system;
+        const systemData = actorData.system;
 
         if (this.type === 'character') {
             this._prepareCharacterData(actorData);
@@ -11,21 +11,5 @@ export class MagicOverflowActor extends Actor {
 
     _prepareCharacterData(actorData) {
         if (actorData.type !== 'character') return;
-    
-        // Make modifications to data here. For example:
-        const systemData = actorData.system;
-
-        // Инициализация стартовых значений
-        // Вычисление производных значений
-        this._prepareResilience(systemData.resilience);
-    }
-
-    _prepareResilience(resilience) {
-        for (let [key, track] of Object.entries(resilience)) {
-            // Убеждаемся что value всегда число
-            track.value = Number(track.value) || 0;
-            // Проверяем не превышает ли значение максимум
-            track.value = Math.min(track.value, track.max);
-        }
     }
 }
