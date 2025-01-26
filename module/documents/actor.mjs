1c6e80b13a3bc4885f34995409d8d16edfba0987
Aleksandr Semenov <a.semenov.job@gmail.com>
2025-01-26 16:09:38 +0100

    Return this back

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -11,5 +11,21 @@ export class MagicOverflowActor extends Actor {
 
     _prepareCharacterData(actorData) {
         if (actorData.type !== 'character') return;
+    
+        // Make modifications to data here. For example:
+        const systemData = actorData.system;
+
+        // Инициализация стартовых значений
+        // Вычисление производных значений
+        this._prepareResilience(systemData.resilience);
+    }
+
+    _prepareResilience(resilience) {
+        for (let [key, track] of Object.entries(resilience)) {
+            // Убеждаемся что value всегда число
+            track.value = Number(track.value) || 0;
+            // Проверяем не превышает ли значение максимум
+            track.value = Math.min(track.value, track.max);
+        }
     }
 }
