68079a6a0b2af1019af80be4a7abece641d04bdc
Aleksandr Semenov <a.semenov.job@gmail.com>
2025-02-01 17:03:54 +0100

    WIP

--- module/dice/magic-roll-dialog.mjs
+++ module/dice/magic-roll-dialog.mjs
@@ -1,134 +1,118 @@
-export class MagicOverflowRoll extends Roll {
-    constructor(formula, data = {}, options = {}) {
-        super(formula, data, options);
-        this.results = {
-            minorSuccess: 0,
-            majorSuccess: 0,
-            overflow: 0
-        };
-        this.actor = data.actor;
+// magic-roll-dialog.mjs
+import { BaseRollDialog } from "./base-roll-dialog.mjs";
+import { MagicOverflowRoll } from "./magic-overflow-roll.mjs";
 
-        // Параметры для магического броска
-        if (data.minorCircles !== undefined) {
-            this.minorCircles = data.minorCircles;
-            this.majorCircles = data.majorCircles;
-            this.filledCircles = {
-                minor: 0,
-                major: 0
-            };
-        }
+export class MagicRollDialog extends BaseRollDialog {
+    constructor(actor, schoolKey, options = {}) {
+        const schoolName = game.i18n.localize(CONFIG.MO.magic.schools[schoolKey]);
+        super(actor, 'magic', schoolName, options);
+        this.schoolKey = schoolKey;
     }
 
-    async evaluate() {
-        await this._evaluate();
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            template: "systems/magic-overflow/templates/dice/magic-roll-dialog.hbs",
+            classes: ["magic-overflow", "dialog", "magic-roll"],
+            width: 500,
+            height: 'auto'
+        });
+    }
 
-        // Подсчитываем успехи
-        this.terms[0].results.forEach(die => {
-            if (die.result === 8) {
-                this.results.overflow++;
-                this.results.minorSuccess++;
-            } else if (die.result === 7) {
-                this.results.majorSuccess++;
-            } else if (die.result === 3) {
-                this.results.minorSuccess++;
-            }
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Обновление значения жертвы при перемещении ползунка
+        html.find('input[name="sacrifice"]').on('input', (event) => {
+            const value = event.currentTarget.value;
+            html.find('.sacrifice-value').text(value);
         });
 
-        // Заполняем круги только для магического броска
-        if (this.minorCircles !== undefined) {
-            this._fillCircles();
+        html.find('form').on('submit', event => this._updateObject(event));
+    }
+
+    getData() {
+        const data = super.getData();
+        data.words = CONFIG.MO.magic.words;
+        // Проверяем владение словами у актора
+        data.wordsProf = Object.entries(this.actor.system.magic.words)
+            .reduce((acc, [key, value]) => {
+                acc[key] = value.prof;
+                return acc;
+            }, {});
+        data.maxSacrifice = 3;
+        return data;
+    }
+
+    getDiceCount(formData) {
+        let diceCount = 1; // Базовый куб
+
+        // Проверяем владение школой
+        if (this.actor.system.magic.schools[this.schoolKey]?.prof) {
+            diceCount += 1;
         }
 
-        // Обрабатываем переполнение для всех бросков
-        if (this.results.overflow > 0 && this.actor) {
-            const currentOverflow = this.actor.system.overflow.value;
-            const maxOverflow = this.actor.system.overflow.max;
-            const newOverflowCount = Math.min(currentOverflow + this.results.overflow, maxOverflow);
+        // Проверяем владение словом
+        const selectedWord = formData.get('word');
+        if (this.actor.system.magic.words[selectedWord]?.prof) {
+            diceCount += 1;
+        }
 
-            if (currentOverflow < maxOverflow) {
-                await this.actor.update({ 'system.overflow.value': newOverflowCount });
+        // Добавляем кубы за связи
+        if (formData.get('personalItem')) diceCount += 1;
+        if (formData.get('trueName')) diceCount += 1;
+        if (formData.get('flesh')) diceCount += 1;
+
+        // Добавляем кубы за жертву
+        const sacrifice = parseInt(formData.get('sacrifice') || 0);
+        diceCount += sacrifice;
+
+        // Добавляем куб за фирменное
+        if (formData.get('signature')) diceCount += 1;
+
+        // Состояния удачи/неудачи
+        if (this.actor.system.state.fortune) {
+            diceCount += 1;
+        }
+        if (this.actor.system.state.misfortune) {
+            diceCount -= 1;
+        }
+
+        return Math.max(0, diceCount);
+    }
+
+    getRollFormula(formData) {
+        const diceCount = this.getDiceCount(formData);
+        return `${diceCount}d8`;
+    }
+
+    async _updateObject(event, formData) {
+        event.preventDefault();
+
+        // Получаем количество кругов из формы
+        const minorCircles = parseInt(formData.minorCircles);
+        const majorCircles = parseInt(formData.majorCircles);
+
+        if (isNaN(minorCircles) || isNaN(majorCircles)) {
+            ui.notifications.error(game.i18n.localize("MO.ui.magicRoll.invalidCircles"));
+            return;
+        }
+
+        const roll = await new MagicOverflowRoll(
+            this.getRollFormula(formData),
+            {
+                actor: this.actor,
+                minorCircles,
+                majorCircles
             }
+        ).evaluate();
 
-            if (currentOverflow >= maxOverflow) {
-                this.overflowMessage = game.i18n.format("MO.ui.overflowChat.trackFull", {
-                    name: this.actor.name
-                });
-            } else if (newOverflowCount === maxOverflow && this.results.overflow > (maxOverflow - currentOverflow)) {
-                this.overflowMessage = game.i18n.format("MO.ui.overflowChat.nowFull", {
-                    name: this.actor.name,
-                    count: this.results.overflow - (maxOverflow - currentOverflow)
-                });
-            }
-        }
-
-        return this;
-    }
-
-    _fillCircles() {
-        // Сначала используем большие успехи
-        let remainingMajor = this.results.majorSuccess;
-
-        // Заполняем большие круги большими успехами
-        const majorFilledByMajor = Math.min(remainingMajor, this.majorCircles);
-        this.filledCircles.major = majorFilledByMajor;
-        remainingMajor -= majorFilledByMajor;
-
-        // Оставшиеся большие успехи используем для малых кругов
-        const minorFilledByMajor = remainingMajor * 2;  // один большой успех может заполнить два малых круга
-
-        // Используем малые успехи для оставшихся малых кругов
-        const remainingMinorCircles = this.minorCircles - minorFilledByMajor;
-        const minorFilledByMinor = Math.min(this.results.minorSuccess, remainingMinorCircles);
-
-        this.filledCircles.minor = minorFilledByMajor + minorFilledByMinor;
-    }
-
-    getSpellResult() {
-        const totalCircles = this.minorCircles + this.majorCircles;
-        const filledCircles = this.filledCircles.minor + this.filledCircles.major;
-        const halfCircles = Math.ceil(totalCircles / 2);
-
-        if (filledCircles >= totalCircles) {
-            return {
-                success: true,
-                message: game.i18n.localize("MO.ui.magicRoll.success")
-            };
-        } else if (filledCircles >= halfCircles) {
-            return {
-                partial: true,
-                message: game.i18n.localize("MO.ui.magicRoll.partial")
-            };
-        } else {
-            return {
-                failure: true,
-                message: game.i18n.localize("MO.ui.magicRoll.failure")
-            };
-        }
-    }
-
-    async render(options = {}) {
-        const templateData = {
-            formula: this.formula,
-            results: this.terms[0].results,
-            successes: this.results,
-            overflowMessage: this.overflowMessage
+        const chatData = {
+            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            flavor: this.getDialogTitle(),
+            content: await roll.render()
         };
 
-        // Добавляем данные только для магического броска
-        if (this.minorCircles !== undefined) {
-            const spellResult = this.getSpellResult();
-            templateData.spellResult = spellResult;
-            templateData.filledCircles = this.filledCircles;
-            templateData.requiredCircles = {
-                minor: this.minorCircles,
-                major: this.majorCircles
-            };
-            templateData.filledMessage = game.i18n.format("MO.ui.magicRoll.filledCircles", {
-                filled: this.filledCircles.minor + this.filledCircles.major,
-                total: this.minorCircles + this.majorCircles
-            });
-        }
-
-        return await renderTemplate("systems/magic-overflow/templates/dice/roll-result.hbs", templateData);
+        await ChatMessage.create(chatData);
+        this.close();
     }
 }
